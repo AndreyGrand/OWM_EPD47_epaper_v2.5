0b47fa2ec61a6b2c422d18158db0fa1317147205
Andrey Gerankin <andrey.gerankin@domrf.ru>
2026-03-18 12:28:24 +0300

    init

---
+++ Lang_ru.h
@@ -0,0 +1,77 @@
+
+#pragma once
+
+
+
+//Temperature - Humidity - Forecast
+const String TXT_FORECAST_VALUES  = "3-дневный прогноз";
+const String TXT_CONDITIONS       = "Conditions";
+const String TXT_DAYS             = "(Дней)";
+const String TXT_TEMPERATURES     = "Температура";
+const String TXT_TEMPERATURE_C    = "Температура (°C)";
+const String TXT_TEMPERATURE_F    = "Температура (°F)";
+const String TXT_HUMIDITY_PERCENT = "Влажность (%)";
+
+// Pressure
+const String TXT_PRESSURE         = "Давление";
+const String TXT_PRESSURE_HPA     = "Давление (hPa)";
+const String TXT_PRESSURE_IN      = "Давление (in)";
+const String TXT_PRESSURE_STEADY  = "Постоянный";
+const String TXT_PRESSURE_RISING  = "Усиливается";
+const String TXT_PRESSURE_FALLING = "Спадает";
+
+//RainFall / SnowFall
+const String TXT_RAINFALL_MM = "Дождь (мм)";
+const String TXT_RAINFALL_IN = "Дождь (in)";
+const String TXT_SNOWFALL_MM = "Снег (мм)";
+const String TXT_SNOWFALL_IN = "Снег (in)";
+const String TXT_PRECIPITATION_SOON = "Ожид.";
+
+
+//Sun
+const String TXT_SUNRISE  = "Восход";
+const String TXT_SUNSET   = "Закат";
+
+//Moon
+const String TXT_MOON_NEW             = "Новый";
+const String TXT_MOON_WAXING_CRESCENT = "Растущий";
+const String TXT_MOON_FIRST_QUARTER   = "Первая четверть";
+const String TXT_MOON_WAXING_GIBBOUS  = "Растущий выпуклый";
+const String TXT_MOON_FULL            = "Полнолуние";
+const String TXT_MOON_WANING_GIBBOUS  = "Убывающий";
+const String TXT_MOON_THIRD_QUARTER   = "Третья четверть";
+const String TXT_MOON_WANING_CRESCENT = "Убывающий полумес";
+
+//Power / WiFi
+const String TXT_POWER  = "Мощность";
+const String TXT_WIFI   = "WiFi";
+const char* TXT_UPDATED = "Обновлён:";
+
+
+//Wind
+const String TXT_WIND_SPEED_DIRECTION = "Ветер скорост/напр";
+const String TXT_N   = "С";
+const String TXT_NNE = "ССВ";
+const String TXT_NE  = "СВ";
+const String TXT_ENE = "ВСВ";
+const String TXT_E   = "В";
+const String TXT_ESE = "ВЮВ";
+const String TXT_SE  = "ЮВ";
+const String TXT_SSE = "ЮЮВ";
+const String TXT_S   = "Ю";
+const String TXT_SSW = "ЮЮЗ";
+const String TXT_SW  = "ЮЗ";
+const String TXT_WSW = "ЗЮЗ";
+const String TXT_W   = "З";
+const String TXT_WNW = "ЗСЗ";
+const String TXT_NW  = "СЗ";
+const String TXT_NNW = "ССЗ";
+
+//Day of the week
+const char* weekday_D[] = {"Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб" };
+
+//Month
+const char* month_M[] = {"Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек" };
+
+//Directions 
+const char* S_Direction[] = {"С", "СВ", "В", "ЮВ", "Ю", "ЮЗ", "З", "СЗ"};
